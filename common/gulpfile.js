dfddf94cda63345fc29fcc1003795ced0d00f8ad
Chunpeng Huo <huocp@me.com>
2019-05-15 14:05:14 +1000

    feat: make gulpfile more adaptive

--- common/gulpfile.js
+++ common/gulpfile.js
@@ -1,6 +1,13 @@
-exports.build = require('./tasks/build');
-exports.clean = require('./tasks/clean');
-exports['clear-cache'] = require('./tasks/clear-cache');
-exports.run = require('./tasks/run');
+const fs = require('fs');
+const path = require('path');
+
+const fps = fs.readdirSync('./tasks');
+for (const p of fps) {
+  // don't load file starts with _xxx
+  if (p.startsWith('_')) continue;
+  // load build.js as exports.build
+  exports[path.parse(p).name] = require('./tasks/' + p);
+}
+
 // default gulp task is "run"
 exports.default = exports.run;
