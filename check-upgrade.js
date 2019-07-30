2964bf1e66d7ce09cedf0759824ac47949e6fce4
Chunpeng Huo <huocp@me.com>
2019-07-30 16:13:48 +1000

    chore: upgrade packages

--- check-upgrade.js
+++ check-upgrade.js
@@ -1,5 +1,6 @@
 // check deps version in all skeleton package.json files
 const fs = require('fs');
+const readline = require('readline');
 const glob = require('glob');
 const ncu = require('npm-check-updates');
 const files = glob.sync('*/package.json');
@@ -11,13 +12,35 @@ for (let file of files) {
 
 async function checkFile(file) {
   console.log('\n' + file);
-  const data = fs.readFileSync(file, 'utf8');
 
   const deps = {};
-  for (const m of data.matchAll(/^\s*"([^"]+)":\s*"([^\w][^"]+)",?\s*$/gm)) {
-    if (m[2].match(/\d+\./)) deps[m[1]] = m[2];
-  }
 
-  const upgrade = await ncu.run({packageData: JSON.stringify({dependencies: deps})});
-  console.log(upgrade);
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: fs.createReadStream(file, 'utf8'), // turn stream into lines
+      crlfDelay: Infinity // be nice to Windows \r\n line break
+    });
+
+    rl.on('line', line => {
+      // ignore empty line
+      line = line.trim();
+      if (!line) return;
+
+      const m = line.match(/^\s*"([^"]+)":\s*"([^\w][^"]+)",?\s*$/);
+      if (!m) return;
+
+      if (m[2].match(/\d+\./)) deps[m[1]] = m[2];
+    });
+
+    rl.on('close', () => {
+      ncu.run({packageData: JSON.stringify({dependencies: deps})})
+      .then(
+        upgrade => {
+          console.log(upgrade);
+          resolve();
+        },
+        reject
+      )
+    });
+  });
 }
