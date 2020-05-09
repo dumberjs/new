ba4a30f54a776522921d9f9b8103f3d4c81edeeb
Chunpeng Huo <huocp@me.com>
2020-05-09 13:23:20 +1000

    test: cleanup

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-const questions = [
+module.exports = [
   {
     message: 'Choose a front-end framework',
     choices: [
@@ -69,15 +69,3 @@ const questions = [
     ]
   }
 ];
-
-if (process.env.CI) {
-  // Add feature "ci" when running in CI environment.
-  questions.push({
-    message: '',
-    choices: [
-      {value: 'ci', title: 'Running in CI environment'}
-    ]
-  });
-}
-
-module.exports = questions;
