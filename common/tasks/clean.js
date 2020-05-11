9bc39fe2d7a7d39611f02624b2712c6a115b4d9b
Chunpeng Huo <huocp@me.com>
2020-05-11 10:54:57 +1000

    feat: simplify to one gulpfile

--- common/tasks/clean.js
+++
@@ -1,6 +0,0 @@
-const del = require('del');
-const {outputDir/* @if plugin */, pluginOutputDir/* @endif */} = require('./_env');
-
-module.exports = function() {
-  return del([outputDir/* @if plugin */, pluginOutputDir/* @endif */]);
-}
